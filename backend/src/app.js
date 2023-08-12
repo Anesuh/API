f59365444ea38daac922e9927e6aaed9282ef133
Heitor Azevedo <heitorazevedo5511@gmail.com>
2023-08-12 19:17:11 -0300

    Adicionando CORS as dependencias

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const app = express();
 const router = require("./router");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 module.exports = app;
