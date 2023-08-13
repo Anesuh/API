1c061c984b78805df0e385c825c625670838029d
Heitor Azevedo <heitorazevedo5511@gmail.com>
2023-08-12 22:25:45 -0300

    Arrumando CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
-//Criando app e requisitando rotas
 const express = require("express");
 const app = express();
 const router = require("./router");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(router);
+
+// Invocando o middleware cors
 app.use(cors());
+
+app.use(router);
+
 module.exports = app;
